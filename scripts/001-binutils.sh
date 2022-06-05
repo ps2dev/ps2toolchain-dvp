7368668ef99e4d7ceaf720e0bd06a6b5eb80d1c5
uyjulian <uyjulian@gmail.com>
2022-06-05 12:48:16 -0500

    Move error condition to function and use it as trap

--- scripts/001-binutils.sh
+++ scripts/001-binutils.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 # binutils-2.14.sh by Francisco Javier Trujillo Mata (fjtrujy@gmail.com)
 
+## Exit with code 1 when any command executed returns a non-zero exit code.
+onerr()
+{
+  exit 1;
+}
+trap onerr ERR
+
 ## Download the source code.
 REPO_URL="https://github.com/ps2dev/binutils-gdb.git"
 REPO_FOLDER="binutils-gdb"
 BRANCH_NAME="dvp-v2.14"
 if test ! -d "$REPO_FOLDER"; then
-  git clone --depth 1 -b "$BRANCH_NAME" "$REPO_URL" && cd "$REPO_FOLDER" || exit 1
+  git clone --depth 1 -b "$BRANCH_NAME" "$REPO_URL"
+  cd "$REPO_FOLDER"
 else
-  cd "$REPO_FOLDER" && git fetch origin && git reset --hard "origin/${BRANCH_NAME}" && git checkout "$BRANCH_NAME" || exit 1
+  cd "$REPO_FOLDER"
+  git fetch origin
+  git reset --hard "origin/${BRANCH_NAME}"
+  git checkout "$BRANCH_NAME"
 fi
 
 TARGET_ALIAS="dvp"
@@ -32,7 +43,9 @@ fi
 PROC_NR=$(getconf _NPROCESSORS_ONLN)
 
 ## Create and enter the toolchain/build directory
-rm -rf "build-$TARGET" && mkdir "build-$TARGET" && cd "build-$TARGET" || { exit 1; }
+rm -rf "build-$TARGET"
+mkdir "build-$TARGET"
+cd "build-$TARGET"
 
 ## Configure the build.
 ../configure \
@@ -41,13 +54,13 @@ rm -rf "build-$TARGET" && mkdir "build-$TARGET" && cd "build-$TARGET" || { exit 
   --target="$TARGET" \
   --disable-nls \
   --disable-build-warnings \
-  $TARG_XTRA_OPTS || { exit 1; }
+  $TARG_XTRA_OPTS
 
 ## Compile and install.
-make --quiet -j "$PROC_NR" clean          || { exit 1; }
-make --quiet -j "$PROC_NR" CFLAGS="$CFLAGS -D_FORTIFY_SOURCE=0 -O2 -Wno-implicit-function-declaration" LDFLAGS="$LDFLAGS -s" || { exit 1; }
-make --quiet -j "$PROC_NR" install        || { exit 1; }
-make --quiet -j "$PROC_NR" clean          || { exit 1; }
+make --quiet -j "$PROC_NR" clean
+make --quiet -j "$PROC_NR" CFLAGS="$CFLAGS -D_FORTIFY_SOURCE=0 -O2 -Wno-implicit-function-declaration" LDFLAGS="$LDFLAGS -s"
+make --quiet -j "$PROC_NR" install
+make --quiet -j "$PROC_NR" clean
 
 ## Exit the build directory.
-cd .. || { exit 1; }
+cd ..
